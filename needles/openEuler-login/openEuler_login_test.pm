f4cd24437ac2e3b20a06c5e2ccc6b97702e9df3d
delete-cloud <1526706882@qq.com>
2024-07-25 20:42:51 +0800

    Update openEuler_login_testsuite

--- needles/openEuler-login/openEuler_login_test.pm
+++ needles/openEuler-login/openEuler_login_test.pm
@@ -12,7 +12,7 @@ sub run {
     # 等待系统启动完成，可以通过检测某个特定的启动标志来实现
     # 例如，等待出现登录界面
     diag "Waiting for login screen to appear";
-    assert_screen 'login', timeout => $timeout;
+    assert_screen 'login', $timeout;
 
     # 输入密码
     diag "Entering password";
@@ -23,9 +23,13 @@ sub run {
     diag "Sending Enter key to login";
     send_key 'ret', timeout => $timeout;
 
+    # 添加额外的等待时间，确保桌面完全加载
+    diag "Waiting for the desktop to load completely";
+    wait_still_screen stilltime => 300, timeout => 300;  # 设置stilltime和timeout为300秒 (5分钟)
+
     # 确认进入桌面
     diag "Checking if desktop is shown";
-    assert_screen 'desktop', timeout => $timeout;
+    assert_screen 'desktop', $timeout;
 
     diag "Login test completed.";
 }
